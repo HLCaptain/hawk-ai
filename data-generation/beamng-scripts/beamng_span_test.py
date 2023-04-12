1b2c8d82ca9d6c098d51ad7720b04e621e1ed614
HLCaptain <pkblazsak@gmail.com>
2023-04-12 12:52:13 +0200

    Updated script to generate data from IMU

--- data-generation/beamng-scripts/beamng_span_test.py
+++ data-generation/beamng-scripts/beamng_span_test.py
@@ -1,15 +1,17 @@
 import random
 import time
 import csv
+import uuid
 
 from beamngpy import BeamNGpy, Scenario, Vehicle, set_up_simple_logging
 from beamngpy.sensors import AdvancedIMU
 from datetime import datetime
 
-def setup_vehicle(vehicle: Vehicle):
-    vehicle.ai.set_mode('span')
-    vehicle.ai.set_aggression(0.2)
+def setup_vehicle(vehicle: Vehicle, aggression: float, mode: str = 'span'):
+    vehicle.ai.set_mode(mode)
+    vehicle.ai.set_aggression(aggression)
     vehicle.ai.drive_in_lane(True)
+    vehicle.ai.set_speed(speed = 130.0, mode = 'limit')
     
 def get_random_vehicle(name: str, model_names: list):
     return Vehicle(
@@ -23,8 +25,8 @@ def main():
     set_up_simple_logging()
 
     number_of_vehicles = 10
-    iterations = 1
-    iteration_duration_in_seconds = 30
+    iterations = 4
+    iteration_duration_in_seconds = 120
     vehicle_model_names = [
         'autobello',
         'midtruck',
@@ -52,25 +54,43 @@ def main():
         'pessima',
         'midsize',
         'wendover'
-    ]
+        ]
     
     csv_name = 'imu_data_' + datetime.now().strftime("%Y_%m_%d_%H_%M_%S") + '.csv'
     with open(csv_name, 'w', newline='') as csvfile:
         
-        fieldnames = ['vehicleId', 'time', 'x', 'y', 'z', 'mass', 'accRaw', 'accSmooth']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+        fieldnames = [
+            'iterationId',
+            'imuId',
+            'vehicleId',
+            'time',
+            'pos',
+            'dirX',
+            'dirY',
+            'dirZ',
+            'angVel',
+            'angAccel',
+            'mass',
+            'accRaw',
+            'accSmooth'
+            ]
+        writer = csv.DictWriter(
+            csvfile,
+            fieldnames = fieldnames,
+            delimiter = ';'
+            )
         writer.writeheader()
-    
-        for iteration in range(1, iterations):
-            
+
+        for iteration in range(iterations):
             random.seed(1337 + time.time_ns())
             beamng = BeamNGpy('localhost', 64256)
-            bng = beamng.open(launch=True)
+            bng = beamng.open(launch = True)
             bng.settings.set_deterministic(60) # Set simulator to 60hz temporal resolution
             
             print('Iteration:', iteration)
             scenario = Scenario('west_coast_usa', 'advanced_IMU_demo', description='Spanning the map with an advanced IMU sensor')
-            vehicle_main = get_random_vehicle('vehicle_main', vehicle_model_names)
+            vehicle_main_name = 'vehicle_main'
+            vehicle_main = get_random_vehicle(vehicle_main_name, vehicle_model_names)
             
             scenario.add_vehicle(vehicle_main, pos=(-717.121, 101, 118.675), rot_quat=(0, 0, 0.3826834, 0.9238795))
             scenario.make(bng)
@@ -85,11 +105,18 @@ def main():
             print('Road data loaded!')
 
             # fetch road data from the game
-            road_ids = random.choices(tuple(roads), k=number_of_vehicles-1)
+            road_ids = random.choices(tuple(roads), k = number_of_vehicles - 1)
             road_edges = {}
 
             for id in road_ids:
                 road_edges[id] = beamng.scenario.get_road_edges(id)
+            
+            aggressions = {}
+            aggressions[vehicle_main_name] = random.uniform(0.2, 1.0)
+            setup_vehicle(
+                vehicle = vehicle_main,
+                aggression = aggressions[vehicle_main_name]
+                )
 
             vehicles_in_scenario = {}
             print(road_edges)  
@@ -97,28 +124,34 @@ def main():
                 r_edges = road_edges[r_id]
                 vehicle_name = 'vehicle_' + r_id
                 vehicle = get_random_vehicle(vehicle_name, vehicle_model_names)
-                middle = r_edges[int(len(r_edges)/2)]['middle']
+                middle = r_edges[int(len(r_edges) / 2)]['middle']
                 scenario.add_vehicle(
                     vehicle,
-                    pos=(
-                        middle[0],
-                        middle[1] + 0.5,
-                        middle[2]
-                    ),
-                    rot_quat=(0, 0, 0.3826834, 0.9238795)
-                )
-                setup_vehicle(vehicle)
+                    pos = (middle[0], middle[1] + 0.5, middle[2]),
+                    rot_quat = (0, 0, 0.3826834, 0.9238795)
+                    )
+                aggressions[vehicle_name] = random.uniform(0.2, 1.0)
+                setup_vehicle(
+                    vehicle = vehicle,
+                    aggression = aggressions[vehicle_name]
+                    )
                 vehicles_in_scenario[r_id] = vehicle
 
-            setup_vehicle(vehicle_main)
             scenario.update()
             bng.scenario.start()
 
-            imus = [AdvancedIMU('vehicle_main_imu', bng, vehicle_main, gfx_update_time=0.01)]
+            vehicle_main_imu_name = 'vehicle_main_imu'
+            imu_vehicles = {vehicle_main_imu_name: vehicle_main_name}
+            
+            imu_update_time = 0.01
+            imus = [AdvancedIMU(vehicle_main_imu_name, bng, vehicle_main, gfx_update_time = imu_update_time)]
             for id in vehicles_in_scenario:
                 vehicle = vehicles_in_scenario[id]
-                imus.append(AdvancedIMU('vehicle_' + id + '_imu', bng, vehicle, gfx_update_time=0.01))        
-
+                vehicle_name = 'vehicle_' + id
+                vehicle_imu_name = vehicle_name + '_imu'
+                imu_vehicles[vehicle_imu_name] = vehicle_name
+                imus.append(AdvancedIMU(vehicle_imu_name, bng, vehicle, gfx_update_time = imu_update_time))
+                
             print('Driving around, polling the advanced IMU sensor at regular intervals...')
             t0 = time.time()
             while time.time() - t0 < iteration_duration_in_seconds:
@@ -128,14 +161,19 @@ def main():
                         # print(data[i])
                         writer.writerow(
                             {
-                                'vehicleId': imu.name,
+                                'iterationId': iteration,
+                                'imuId': imu.name,
+                                'vehicleId': imu_vehicles[imu.name],
                                 'time': data[i]['time'],
-                                'x': data[i]['pos'][0],
-                                'y': data[i]['pos'][1],
-                                'z': data[i]['pos'][2],
+                                'pos': data[i]['pos'],
+                                'dirX': data[i]['dirX'],
+                                'dirY': data[i]['dirY'],
+                                'dirZ': data[i]['dirZ'],
+                                'angVel': data[i]['angVel'],
+                                'angAccel': data[i]['angAccel'],
                                 'mass': data[i]['mass'],
                                 'accRaw': data[i]['accRaw'],
-                                'accSmooth': data[i]['accSmooth']
+                                'accSmooth': data[i]['accSmooth'],
                             }
                         )
 
